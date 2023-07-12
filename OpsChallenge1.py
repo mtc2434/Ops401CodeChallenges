31d4fcc855c3ac59a804d7d72a1cd1ec67638ae2
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-07-12 11:06:38 -0700

    Update OpsChallenge1.py

--- OpsChallenge1.py
+++ OpsChallenge1.py
@@ -1,1 +1,13 @@
-placehold
+#!/usr/bin/env python3
+# Script:  OpsChallenge1.py                     
+# Author:  Marcelo Clark                      
+# Date of latest revision:   6/12/2023  
+import os
+import time
+import datetime
+
+def ping(host):
+    #pings host once
+    response = os.system("ping -c 1 " + host)  
+    #The output is true if it is equal to 0
+    return response == 0  
