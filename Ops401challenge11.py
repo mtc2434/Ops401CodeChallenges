a0d6ec29dca6680768a3a2554b33bab8e8c40ae0
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-07-25 08:18:00 -0700

    Update Ops401challenge11.py

--- Ops401challenge11.py
+++ Ops401challenge11.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+# Script:  OpsChallenge11.py                     
+# Author:  Marcelo Clark                      
+# Date of latest revision:   7/25/2023 
+from scapy.all import *
 
+def scan_tcp_port_range(host, port_range):
+    open_ports = []
+    closed_ports = []
+
+    for port in port_range:
+        response = sr1(IP(dst=host)/TCP(dport=port, flags="S"), timeout=1, verbose=0)
